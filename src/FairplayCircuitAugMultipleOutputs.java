27909c98e651fdeea8bb6fd00f781a9b881f7010
Roberto Trifiletti <roberto.trifiletti@gmail.com>
2012-09-13 21:08:01 +0200

    Minor changes

--- src/FairplayCircuitAugMultipleOutputs.java
+++ src/FairplayCircuitAugMultipleOutputs.java
@@ -24,27 +24,25 @@ public class FairplayCircuitAugMultipleOutputs implements Runnable {
 	public void run() {
 		List<Gate> parsedGates = circuitParser.getGates();
 
-		
 		int n1 = circuitParser.getNumberOfP1Inputs();
 		int n2 = circuitParser.getNumberOfP2Inputs();
 		int m1 = circuitParser.getNumberOfP1Outputs();
 		int m2 = circuitParser.getNumberOfP2Outputs();
 		int actualNumberOfWires = 
 				circuitParser.getWireCountFromSingleList(parsedGates);
-		int newNumberOfInputs = n1 + 3*m1 + n2;
+		int addedInput = 3 *m1;
+		int newNumberOfInputs = n1 + addedInput + n2;
 		int newNumberOfOutputs = 2*m1 + m2;
 
 		List<Gate> parsedGatesAddedInputs = 
-				getAdditionalInputWires(parsedGates, n1, 3*m1);
-		
-		int startOfCInput = n1 + 2*m1;
-		int startOfP1Output = actualNumberOfWires - n1 - n2 + 3*m1; //to account for additional inputs
-		int startOfP2Output = actualNumberOfWires - n2 + 3*m1; //to account for additional inputs
+				getAdditionalInputWires(parsedGates, n1, addedInput);
 
-//		writeOutput(parsedGatesAddedInputs);
-		
+		int startOfCInput = n1 + 2*m1;
+		int startOfP1Output = actualNumberOfWires - n1 - n2 + addedInput; //to account for added inputs
+		int startOfP2Output = actualNumberOfWires - n2 + addedInput; //to account for added inputs
+
 		List<Gate> nonOutputPositionedEGates = getEGates(startOfP1Output, startOfCInput, m1);
-		
+
 		String[] headers = circuitParser.getNewFairplayHeader(nonOutputPositionedEGates);
 		CommonUtilities.outputFairplayCircuit(nonOutputPositionedEGates, 
 				outputFile, headers);
@@ -71,7 +69,7 @@ public class FairplayCircuitAugMultipleOutputs implements Runnable {
 
 	private List<Gate> getEGates(int startOfP1Outputs, int startOfC, int m1) {
 		List<Gate> res = new ArrayList<Gate>();
-		
+
 		for(int i = 0; i < m1; i++){
 			int leftWire = startOfP1Outputs + i;
 			int rightWire = startOfC + i;
@@ -84,26 +82,26 @@ public class FairplayCircuitAugMultipleOutputs implements Runnable {
 		return res;
 	}
 
-//	private List<List<Gate>> getOriginalOutputGates(List<Gate> parsedGates, int p1Outputs, 
-//			int p2Outputs) {
-//		List<List<Gate>> res = new ArrayList<List<Gate>>();
-//		List<Gate> p1OutputGates = new ArrayList<Gate>();
-//		List<Gate> p2OutputGates = new ArrayList<Gate>();
-//		int actualNumberOfWires = circuitParser.getParsedWireCount();
-//		int totalOutputs = p1Outputs + p2Outputs;
-//
-//		for(Gate g: parsedGates){	
-//			if (g.getOutputWireIndex() >= actualNumberOfWires - totalOutputs &&
-//					g.getOutputWireIndex() < actualNumberOfWires - p2Outputs){
-//				p1OutputGates.add(g);
-//			}
-//			else if(g.getOutputWireIndex() >= actualNumberOfWires - totalOutputs
-//					&& g.getOutputWireIndex() >= actualNumberOfWires - p2Outputs){
-//				p2OutputGates.add(g);
-//			}
-//		}
-//		res.add(p1OutputGates);
-//		res.add(p2OutputGates);
-//		return res;
-//	}
+	//	private List<List<Gate>> getOriginalOutputGates(List<Gate> parsedGates, int p1Outputs, 
+	//			int p2Outputs) {
+	//		List<List<Gate>> res = new ArrayList<List<Gate>>();
+	//		List<Gate> p1OutputGates = new ArrayList<Gate>();
+	//		List<Gate> p2OutputGates = new ArrayList<Gate>();
+	//		int actualNumberOfWires = circuitParser.getParsedWireCount();
+	//		int totalOutputs = p1Outputs + p2Outputs;
+	//
+	//		for(Gate g: parsedGates){	
+	//			if (g.getOutputWireIndex() >= actualNumberOfWires - totalOutputs &&
+	//					g.getOutputWireIndex() < actualNumberOfWires - p2Outputs){
+	//				p1OutputGates.add(g);
+	//			}
+	//			else if(g.getOutputWireIndex() >= actualNumberOfWires - totalOutputs
+	//					&& g.getOutputWireIndex() >= actualNumberOfWires - p2Outputs){
+	//				p2OutputGates.add(g);
+	//			}
+	//		}
+	//		res.add(p1OutputGates);
+	//		res.add(p2OutputGates);
+	//		return res;
+	//	}
 }
