6321753ea52e3cdf15d66a35efaf532c5a322230
Roberto Trifiletti <roberto.trifiletti@gmail.com>
2012-12-09 10:33:05 +0100

    Fixed the augmentation

--- src/FairplayCircuitAugChecksum.java
+++ src/FairplayCircuitAugChecksum.java
@@ -13,12 +13,17 @@ public class FairplayCircuitAugChecksum implements Runnable {
 	private int largestAugOutputWire;
 	private int l;
 	private List<Gate> outputGates;
+	private int numberOfOriginalInputs;
+	private int t_a;
+	private int numberOfNewInputs;
+	private int r;
 
 	public FairplayCircuitAugChecksum(FairplayCircuitParser circuitParser,
 			File outputFile, int l){
 		this.outputFile = outputFile;
 		this.circuitParser = circuitParser;
 		this.l = l;
+		
 		outputGates = new ArrayList<Gate>();
 		numberOfNonXORGatesAdded = 0;
 		largestOutputWire = 0;
@@ -27,16 +32,16 @@ public class FairplayCircuitAugChecksum implements Runnable {
 	@Override
 	public void run() {
 		List<Gate> parsedGates = circuitParser.getGates();
-		int numberOfOriginalInputs = circuitParser.getNumberOfInputs();
-		int t_a = circuitParser.getNumberOfP1Inputs();
-		int newNumberOfInputs = numberOfOriginalInputs + t_a + 2 * l;
+		
+		// Must be set after call to circuitParser
+		numberOfOriginalInputs = circuitParser.getNumberOfInputs();
+		t_a = circuitParser.getNumberOfP1Inputs();
+		numberOfNewInputs = numberOfOriginalInputs + t_a + 2 * l - 1;
+		r = numberOfNewInputs - t_a - (l - 1);
 
-		List<Gate> augGates = 
-				getAugGates(newNumberOfInputs, t_a);
-
+		List<Gate> augGates = getAugGates();
 		List<Gate> incrementedGates = 
-				getIncrementedGates(parsedGates, newNumberOfInputs, 
-						numberOfOriginalInputs, t_a);
+				getIncrementedGates(parsedGates);
 		List<Gate> augOutputGates = getAugOutputGates(); //Must be called after getIncrementedGates()
 
 		List<Gate> augCircuit = new ArrayList<Gate>();
@@ -49,10 +54,10 @@ public class FairplayCircuitAugChecksum implements Runnable {
 		CommonUtilities.outputFairplayCircuit(augCircuit, outputFile, headers);
 	}
 
-	private List<Gate> getAugGates(int newNumberOfInputs, int t_a) {
+	private List<Gate> getAugGates() {
 		List<Gate> res = new ArrayList<Gate>();
 		int gateNumber = 0;
-		int r = newNumberOfInputs - t_a - l;
+		
 		List<Gate> andGates = new ArrayList<Gate>();
 
 		/**
@@ -62,7 +67,7 @@ public class FairplayCircuitAugChecksum implements Runnable {
 			for (int x = 0; x < t_a; x++) {
 				int leftWire = r + i + x;
 				int rightWire = x;
-				int outputWire = newNumberOfInputs + i * t_a + x;
+				int outputWire = numberOfNewInputs + i * t_a + x;
 				Gate g = new Gate("2 1 "+ leftWire + " " + rightWire +
 						" " + outputWire + " 0001");
 				largestAugOutputWire = Math.max(largestAugOutputWire, g.getOutputWireIndex());
@@ -85,11 +90,12 @@ public class FairplayCircuitAugChecksum implements Runnable {
 		int xorWireStart = largestAugOutputWire + 1;
 		for (int i = 0; i < l; i++) {
 			int priorOutputWire = 0;
+			
 			for (int x = 1; x < t_a; x++) { // Start x from 1 because we need one less xor per layer
 				int leftWire;
-				int rightWire = newNumberOfInputs + t_a * i + x;
+				int rightWire = numberOfNewInputs + t_a * i + x;
 				if (x == 1){
-					leftWire = newNumberOfInputs + t_a * i + x - 1;
+					leftWire = numberOfNewInputs + t_a * i + x - 1;
 				} else {
 					leftWire = priorOutputWire;
 				}
@@ -110,7 +116,6 @@ public class FairplayCircuitAugChecksum implements Runnable {
 			//with the current bit of s. The output of this xor is the s'th
 			//output bit of the augmentation output.
 			Gate xorOutputGate = xorGates.get(xorGates.size() - 1);
-
 			int xorOutputWireIndex = xorOutputGate.getOutputWireIndex();
 
 			int s = t_a + i;
@@ -125,8 +130,7 @@ public class FairplayCircuitAugChecksum implements Runnable {
 
 	}
 
-	private List<Gate> getIncrementedGates(List<Gate> parsedGates, 
-			int newNumberOfInputs, int numberOfOriginalInputs, int t_a){
+	private List<Gate> getIncrementedGates(List<Gate> parsedGates){
 		List<Gate> res = new ArrayList<Gate>();
 		int incNumber = largestAugOutputWire + 1 - numberOfOriginalInputs;
 
@@ -143,7 +147,7 @@ public class FairplayCircuitAugChecksum implements Runnable {
 			} else if (leftWireIndex >= numberOfOriginalInputs) {
 				g.setLeftWireIndex(leftWireIndex + incNumber);
 			}
-			
+
 			// Same, but for right wire
 			int rightWireIndex = g.getRightWireIndex();
 			if (rightWireIndex < numberOfOriginalInputs &&
@@ -152,7 +156,7 @@ public class FairplayCircuitAugChecksum implements Runnable {
 			} else if (rightWireIndex >= numberOfOriginalInputs) {
 				g.setRightWireIndex(g.getRightWireIndex() + incNumber);
 			}
-			
+
 			// Output wires should always be incremented
 			int newIndex = g.getOutputWireIndex() + incNumber;
 			g.setOutputWireIndex(newIndex);
@@ -172,18 +176,13 @@ public class FairplayCircuitAugChecksum implements Runnable {
 
 	private String[] getHeaders(List<Gate> augCircuit) {
 		String[] res = new String[2];
-		String[] inputOutputInfo = 
-				circuitParser.getFairplayInputOutputHeader();
 
-		int t_a = Integer.parseInt(inputOutputInfo[0]);
+		res[0] = augCircuit.size() + " " + CommonUtilities.getWireCount(augCircuit);
 
-		// TODO Hack until figured out about last input used or not
-		res[0] = augCircuit.size() + " " + (CommonUtilities.getWireCount(augCircuit) + 1);
-
-		int newP1Input = Integer.parseInt(inputOutputInfo[0]) + l;
-		int newP2Input = Integer.parseInt(inputOutputInfo[1]) + t_a + l;
-		int newP1Output = Integer.parseInt(inputOutputInfo[2]);
-		int newP2Output = Integer.parseInt(inputOutputInfo[3]) + l;
+		int newP1Input = circuitParser.getNumberOfP1Inputs() + l;
+		int newP2Input = circuitParser.getNumberOfP2Inputs() + t_a + l - 1;
+		int newP1Output = circuitParser.getNumberOfP1Outputs();
+		int newP2Output = circuitParser.getNumberOfP2Outputs() + l;
 
 		res[1] = newP1Input + " " + newP2Input + " " + newP1Output + " " +
 				newP2Output;
