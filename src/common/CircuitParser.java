d1a5df6da7db0725940886db168f8fad8d7f774e
Roberto Trifiletti <roberto.trifiletti@gmail.com>
2013-02-27 15:09:48 +0100

    Expanded the interface

--- src/common/CircuitParser.java
+++ src/common/CircuitParser.java
@@ -1,7 +1,16 @@
 package common;
 
 import java.io.File;
+import java.util.List;
 
 public interface CircuitParser<E> extends CircuitProvider<E> {
 	public File getCircuitFile();
+	public List<E> getGates();
+	public String[] getHeaders();
+	public int getNumberOfInputs();
+	public int getNumberOfOutputs();
+	public int getNumberOfNonXORGates();
+	public int getNumberOfP1Inputs();
+	public int getNumberOfP2Inputs();
+	public int getNumberOfWires();
 }
